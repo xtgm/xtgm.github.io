3d8a1b17e9de22c642b16d83b8d11e11632fa3fc
xtgm <zhangx0235@outlook.com>
2025-12-28 17:00:31 +0800

    Site updated: 2025-12-28 17:00:31

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/12/24/stallTCP1-3V1后台版图文教程/","2025/12/24/hello-world/"];function toRandomPost(){
+var posts=["undefined/stallTCP1-3V1后台版图文教程/","undefined/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
